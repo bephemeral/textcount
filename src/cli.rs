10b15f6cf7c50bd7a893a20a1486fd602ec5dce6
bephemeral <244405225+bephemeral@users.noreply.github.com>
2025-11-16 23:03:34 -0500

    feat: implement counters

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,8 @@
 use clap::Parser;
 use std::{fs::read_to_string, io};
 
+use crate::counters::*;
+
 /// Print newline, word, and byte counts for each FILE, and a total line if
 /// more than one FILE is specified.  A word is a non-zero-length sequence of
 /// printable characters delimited by white space. {n}
@@ -46,8 +48,8 @@ fn read_from_stdin() -> io::Result<Content> {
     })
 }
 
-pub fn get_content(args: Args) -> io::Result<Vec<Content>> {
-    if let Some(files) = args.file {
+pub fn get_content(args: &Args) -> io::Result<Vec<Content>> {
+    if let Some(files) = &args.file {
         files
             .iter()
             .map(|file| {
@@ -61,4 +63,28 @@ pub fn get_content(args: Args) -> io::Result<Vec<Content>> {
     } else {
         Ok(vec![read_from_stdin()?])
     }
+}
+
+pub fn display_content(args: &Args, content: &Content) {
+    if args.lines {
+        print!("{} ", count_lines(&content.text));
+    }
+
+    if args.words {
+        print!("{} ", count_words(&content.text));
+    }
+
+    if args.chars {
+        print!("{} ", count_chars(&content.text));
+    }
+
+    if args.bytes {
+        print!("{} ", count_bytes(&content.text));
+    }
+
+    if args.max_line_length {
+        print!("{} ", get_max_line_length(&content.text));
+    }
+
+    println!("{}", content.source);
 }
